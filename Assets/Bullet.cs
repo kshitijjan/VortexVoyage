1129c1638bd02209f8f376e69308ec86438baac6
Kshitij Jain <kshitiz.jain636@gmail.com>
2024-04-20 10:42:23 +0530

    vortexvoyage

---
+++ Assets/Bullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public Vector2 direction = new Vector2(1, 0);
+    public float speed = 2;
+
+    public Vector2 velocity;
+
+    public bool isEnemy = false;
+
+    void Start()
+    {
+        Destroy(gameObject, 3);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        velocity = direction * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        pos += velocity * Time.fixedDeltaTime;
+
+        transform.position = pos;
+    }
+}
